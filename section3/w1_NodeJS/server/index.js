bc87ff6a47761ef573a4904050f120979478df37
laoxie <jeroo@qq.com>
2021-11-02 12:10:33 +0800

    day2-2 websocket

--- section3/w1_NodeJS/server/index.js
+++ section3/w1_NodeJS/server/index.js
@@ -37,3 +37,42 @@ const PORT = 2108;
 app.listen(PORT,()=>{
     console.log('server is running at port ' + PORT)
 })
+
+
+
+// websocket服务器
+let socketServer = require('ws').Server;
+let wsServer = new socketServer({
+    port: 1001
+});
+
+// 监听客户端连接: 当客户端连接websocket服务器时自动自动connection时间
+wsServer.on('connection',(client)=>{
+    // client: 客户端对象，每个客户端对象会自动保存到wsServer.clients属性中
+
+    // 监听客户端对象操作：发送消息，关闭连接
+    client.on('message',(msg)=>{
+        // 当客户端给服务器发送消息时执行这里的代码
+        console.log('message',msg.toString())
+
+        // 广播：收到某一个客户端的消息后转发给其他的客户端
+        wsServer.clients.forEach(item=>{
+            const message = msg.toString()
+            // if(item != client){
+                item.send(message)
+            // }
+        })
+    })
+
+    client.on('close',()=>{
+        // 如果客户端管理连接，自动自动这里的代码
+        console.log('close')
+    })
+
+
+    // 服务器给客户端发送消息
+    client.send('hello my name is 服务器')
+    
+})
+
+// wsServer.clients
